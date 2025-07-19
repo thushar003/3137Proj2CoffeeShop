fd3702a05000fc230b55a4e3304ebf447d1cab85
thushar003 <thusharjoji2@gmail.com>
2025-07-19 19:33:40 -0400

    Added IBrewer, Drink, brief code added

---
+++ 3137Proj2CoffeeShop/Drink.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3137Proj2CoffeeShop
+{
+    public abstract class Drink
+    {
+        public abstract void Prepare();
+        public abstract decimal Cost();
+        public abstract string Description();
+    }
+}
---
+++ 3137Proj2CoffeeShop/IBrewer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3137Proj2CoffeeShop
+{
+    public interface IBrewer
+    {
+        abstract void Brew(string description);
+    }
+}
